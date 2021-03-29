f9846774b7dfebe2e8de0c630be0bb04a868aa87
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2021-03-29 17:17:47 +0900

    release

---
+++ cmd/send-anything/app/client.go
@@ -0,0 +1,202 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+
+	"bitbucket.org/latonaio/aion-core/pkg/log"
+	"bitbucket.org/latonaio/aion-core/proto/kanbanpb"
+	"github.com/avast/retry-go"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+)
+
+func sendToServerNotifyFailure(stream kanbanpb.SendAnything_SendToOtherDevicesClient) error {
+	req := &kanbanpb.SendContext{
+		Code:    kanbanpb.UploadRequestCode_SendingFile_FAILED,
+		Context: nil,
+	}
+	if err := sendToServer(stream, req); err != nil {
+		return err
+	}
+	return nil
+}
+
+func sendToServer(stream kanbanpb.SendAnything_SendToOtherDevicesClient, req *kanbanpb.SendContext) error {
+	const connRetryCount = 10
+	doFunc := func() error {
+		return stream.Send(req)
+	}
+	delayFunc := func(n uint, config *retry.Config) time.Duration {
+		log.Warnf("[SendToOtherDevices client] retry to send because failed to send buffer ")
+		return time.Second
+	}
+
+	if err := retry.Do(doFunc, retry.DelayType(delayFunc), retry.Attempts(connRetryCount)); err != nil {
+		log.Errorf("[SendToOtherDevices client] failed to send chunk(error: %v)", err)
+		return err
+	}
+	return nil
+}
+
+func sendToOtherDeviceClient(ctx context.Context, m *kanbanpb.SendKanban, port int) error {
+	// connect to remote device
+	deviceAddr := m.DeviceAddr + ":" + strconv.Itoa(port)
+	conn, err := grpc.DialContext(ctx, deviceAddr, grpc.WithInsecure())
+	if err != nil {
+		err := errors.Wrap(err, fmt.Sprintf("cannot connect to remote device: %s", deviceAddr))
+		log.Print(err)
+		return err
+	}
+	defer conn.Close()
+
+	log.Printf("[SendToOtherDevice client] success to connect : %s", deviceAddr)
+
+	client := kanbanpb.NewSendAnythingClient(conn)
+	stream, err := client.SendToOtherDevices(ctx)
+	if err != nil {
+		err := errors.Wrap(err, fmt.Sprintf("cannot connect to remote device: %s", deviceAddr))
+		log.Print(err)
+		return err
+	}
+
+	// close stream
+	defer func() {
+		reply, err := stream.CloseAndRecv()
+		if err != nil {
+			log.Printf("[SendToOtherDevice client] received eos ack is failed: %s: %v", deviceAddr, err)
+			return
+		}
+		if reply.StatusCode != kanbanpb.UploadStatusCode_OK {
+			log.Printf("[SendToOtherDevice client] response error message: %s", reply.Message)
+			return
+		}
+		log.Printf("[SendToOtherDevice client] success to send kanban and files: %s", deviceAddr)
+	}()
+
+	// send kanban and files
+	if err := sendKanban(stream, m); err != nil {
+		return errors.Wrap(err, fmt.Sprintf("[SendToOtherDevice client] sending kanban is failed: %s", deviceAddr))
+	}
+	if err := sendFileList(stream, m); err != nil {
+		return errors.Wrap(err, fmt.Sprintf("[SendToOtherDevice client] sending files are failed: %s", deviceAddr))
+	}
+	if err := sendEos(stream); err != nil {
+		return errors.Wrap(err, fmt.Sprintf("[SendToOtherDevice client] sending eos is failed: %s", deviceAddr))
+	}
+	return nil
+}
+
+// send kanban to other devices
+func sendKanban(stream kanbanpb.SendAnything_SendToOtherDevicesClient, m *kanbanpb.SendKanban) error {
+	log.Printf("[SendToOtherDevices client] start to send kanban: %s", m.NextService)
+	cont, err := ptypes.MarshalAny(m)
+	if err != nil {
+		return err
+	}
+
+	req := &kanbanpb.SendContext{
+		Code:    kanbanpb.UploadRequestCode_SendingKanban,
+		Context: cont,
+	}
+	if err := stream.Send(req); err != nil {
+		return err
+	}
+	log.Printf("[SendToOtherDevices client] finish to send kanban: %s", m.NextService)
+	return nil
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
+
+// send data to other devices
+func sendFile(stream kanbanpb.SendAnything_SendToOtherDevicesClient, fileName string, filePath string) error {
+	log.Printf("[SendToOtherDevices client] start to send file: %s, %s", fileName, filePath)
+	if err := checkFileStatus(filePath); err != nil {
+		return err
+	}
+	f, err := os.Open(filePath)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("cant open file: %s", filePath))
+	}
+	defer f.Close()
+
+	for {
+		var req *kanbanpb.SendContext
+		chunk := make([]byte, chunkSize)
+		count, err := f.Read(chunk)
+		if err != nil {
+			if err != io.EOF {
+				return sendToServerNotifyFailure(stream)
+			}
+			req = &kanbanpb.SendContext{
+				Code:    kanbanpb.UploadRequestCode_SendingFile_EOF,
+				Context: nil,
+			}
+			if err := sendToServer(stream, req); err != nil {
+				return sendToServerNotifyFailure(stream)
+			}
+			break
+		}
+		anyChunk, err := ptypes.MarshalAny(&kanbanpb.Chunk{
+			Context: chunk[:count],
+			Name:    fileName,
+		})
+		if err != nil {
+			return sendToServerNotifyFailure(stream)
+		}
+		req = &kanbanpb.SendContext{
+			Code:    kanbanpb.UploadRequestCode_SendingFile_CONT,
+			Context: anyChunk,
+		}
+		if err := sendToServer(stream, req); err != nil {
+			return sendToServerNotifyFailure(stream)
+		}
+	}
+	log.Printf("[SendToOtherDevices client] finish to send file: %s, %s", fileName, filePath)
+	return nil
+}
+
+// send data by file list to other devices
+func sendFileList(stream kanbanpb.SendAnything_SendToOtherDevicesClient, m *kanbanpb.SendKanban) error {
+	var errStr []string
+	for _, fileName := range m.AfterKanban.FileList {
+		fileExists := fileExists(fileName)
+		if fileExists {
+			filePath := fileName
+			if err := sendFile(stream, fileName, filePath); err != nil {
+				errStr = append(errStr, err.Error())
+			}
+		} else {
+			filePath := path.Join(m.AfterKanban.DataPath, fileName)
+			if err := sendFile(stream, fileName, filePath); err != nil {
+				errStr = append(errStr, err.Error())
+			}
+		}
+	}
+	if len(errStr) != 0 {
+		return fmt.Errorf("cant send files\n%s", strings.Join(errStr, "\n"))
+	}
+	return nil
+}
+
+// send eos to other devices
+func sendEos(stream kanbanpb.SendAnything_SendToOtherDevicesClient) error {
+	req := &kanbanpb.SendContext{
+		Code:    kanbanpb.UploadRequestCode_EOS,
+		Context: nil,
+	}
+	if err := stream.Send(req); err != nil {
+		return err
+	}
+	return nil
+}
--- cmd/send-anything/app/server.go
+++ cmd/send-anything/app/server.go
@@ -3,21 +3,18 @@ package app
 import (
 	"context"
 	"fmt"
-	"io"
 	"net"
 	"os"
 	"os/signal"
 	"path"
 	"path/filepath"
 	"strconv"
-	"strings"
 	"syscall"
 	"time"
 
 	"bitbucket.org/latonaio/aion-core/pkg/common"
 	"bitbucket.org/latonaio/aion-core/pkg/log"
 	"bitbucket.org/latonaio/aion-core/proto/kanbanpb"
-	"github.com/avast/retry-go"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
@@ -72,7 +69,8 @@ func (srv *Server) createServerConnection() error {
 
 // callback that connect with service broker
 func (srv *Server) ServiceBrokerConn(stream kanbanpb.SendAnything_ServiceBrokerConnServer) error {
-	ctx := stream.Context()
+	ctx, cancel := context.WithCancel(stream.Context())
+	defer cancel()
 	log.Printf("connect from servicebroker")
 
 	go func() {
@@ -84,15 +82,21 @@ func (srv *Server) ServiceBrokerConn(stream kanbanpb.SendAnything_ServiceBrokerC
 				return
 			}
 			log.Printf("[ServiceBrokerConn] receive from service broker")
-			if err := srv.sendToOtherDeviceClient(ctx, in); err != nil {
+			if err := sendToOtherDeviceClient(ctx, in, srv.env.ServerPort); err != nil {
 				log.Printf("%v", err)
 			}
 		}
 	}()
 
 	// start loop that send request to service broker
-	go func() {
-		for k := range srv.sendToServiceBrokerCh {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case k, ok := <-srv.sendToServiceBrokerCh:
+			if !ok {
+				return nil
+			}
 			if err := stream.Send(k); err != nil {
 				log.Printf("[ServiceBrokerConn] grpc send error: %v", err)
 			} else {
@@ -100,118 +104,7 @@ func (srv *Server) ServiceBrokerConn(stream kanbanpb.SendAnything_ServiceBrokerC
 					k.DeviceName, k.NextService)
 			}
 		}
-	}()
-
-	<-ctx.Done()
-	return nil
-}
-
-// connect to send anything in other devices amd send data
-func (srv *Server) sendToOtherDeviceClient(ctx context.Context, m *kanbanpb.SendKanban) error {
-	// connect to remote device
-	deviceAddr := m.DeviceAddr + ":" + strconv.Itoa(srv.env.GetClientPort())
-	conn, err := grpc.DialContext(ctx, deviceAddr, grpc.WithInsecure())
-	defer conn.Close()
-
-	if err != nil {
-		err := errors.Wrap(err, fmt.Sprintf("cannot connect to remote device: %s", deviceAddr))
-		log.Print(err)
-		return err
 	}
-	log.Printf("[SendToOtherDevice client] success to connect : %s", deviceAddr)
-
-	client := kanbanpb.NewSendAnythingClient(conn)
-	stream, err := client.SendToOtherDevices(ctx)
-	if err != nil {
-		err := errors.Wrap(err, fmt.Sprintf("cannot connect to remote device: %s", deviceAddr))
-		log.Print(err)
-		return err
-	}
-
-	// close stream
-	defer func() {
-		reply, err := stream.CloseAndRecv()
-		if err != nil {
-			log.Printf("[SendToOtherDevice client] received eos ack is failed: %s: %v", deviceAddr, err)
-			return
-		}
-		if reply.StatusCode != kanbanpb.UploadStatusCode_OK {
-			log.Printf("[SendToOtherDevice client] response error message: %s", reply.Message)
-			return
-		}
-		log.Printf("[SendToOtherDevice client] success to send kanban and files: %s", deviceAddr)
-	}()
-
-	// send kanban and files
-	if err := sendKanban(stream, m); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("[SendToOtherDevice client] sending kanban is failed: %s", deviceAddr))
-	}
-	if err := sendFileList(stream, m); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("[SendToOtherDevice client] sending files are failed: %s", deviceAddr))
-	}
-	if err := sendEos(stream); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("[SendToOtherDevice client] sending eos is failed: %s", deviceAddr))
-	}
-	return nil
-}
-
-// send eos to other devices
-func sendEos(stream kanbanpb.SendAnything_SendToOtherDevicesClient) error {
-	req := &kanbanpb.SendContext{
-		Code:    kanbanpb.UploadRequestCode_EOS,
-		Context: nil,
-	}
-	if err := stream.Send(req); err != nil {
-		return err
-	}
-	return nil
-}
-
-// send kanban to other devices
-func sendKanban(stream kanbanpb.SendAnything_SendToOtherDevicesClient, m *kanbanpb.SendKanban) error {
-	log.Printf("[SendToOtherDevices client] start to send kanban: %s", m.NextService)
-	cont, err := ptypes.MarshalAny(m)
-	if err != nil {
-		return err
-	}
-
-	req := &kanbanpb.SendContext{
-		Code:    kanbanpb.UploadRequestCode_SendingKanban,
-		Context: cont,
-	}
-	if err := stream.Send(req); err != nil {
-		return err
-	}
-	log.Printf("[SendToOtherDevices client] finish to send kanban: %s", m.NextService)
-	return nil
-}
-
-func fileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	return err == nil
-}
-
-// send data by file list to other devices
-func sendFileList(stream kanbanpb.SendAnything_SendToOtherDevicesClient, m *kanbanpb.SendKanban) error {
-	var errStr []string
-	for _, fileName := range m.AfterKanban.FileList {
-		fileExists := fileExists(fileName)
-		if fileExists {
-			filePath := fileName
-			if err := sendFile(stream, fileName, filePath); err != nil {
-				errStr = append(errStr, err.Error())
-			}
-		} else {
-			filePath := path.Join(m.AfterKanban.DataPath, fileName)
-			if err := sendFile(stream, fileName, filePath); err != nil {
-				errStr = append(errStr, err.Error())
-			}
-		}
-	}
-	if len(errStr) != 0 {
-		return fmt.Errorf("cant send files\n%s", strings.Join(errStr, "\n"))
-	}
-	return nil
 }
 
 func checkFileStatus(filePath string) error {
@@ -241,74 +134,10 @@ func checkFileStatus(filePath string) error {
 	return nil
 }
 
-// send data to other devices
-func sendFile(stream kanbanpb.SendAnything_SendToOtherDevicesClient, fileName string, filePath string) error {
-	log.Printf("[SendToOtherDevices client] start to send file: %s, %s", fileName, filePath)
-	if err := checkFileStatus(filePath); err != nil {
-		return err
-	}
-	f, err := os.Open(filePath)
-	defer f.Close()
-	if err != nil {
-		return errors.Wrap(err, fmt.Sprintf("cant open file: %s", filePath))
-	}
-
-	req := &kanbanpb.SendContext{
-		Code:    kanbanpb.UploadRequestCode_SendingFile_CONT,
-		Context: nil,
-	}
-	chunk := &kanbanpb.Chunk{
-		Context: make([]byte, chunkSize),
-		Name:    fileName,
-	}
-
-	stat, err := f.Stat()
-	if err != nil {
-		return errors.Wrap(err, fmt.Sprintf("cant get file stat: %s", filePath))
-	}
-	fileBuf := make([]byte, stat.Size())
-	if _, err := f.Read(fileBuf); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("cant read file: %s", filePath))
-	}
-	for currentByte := 0; currentByte < len(fileBuf); currentByte += chunkSize {
-		if currentByte+chunkSize > len(fileBuf) {
-			req.Code = kanbanpb.UploadRequestCode_SendingFile_EOF
-			chunk.Context = fileBuf[currentByte:]
-		} else {
-			chunk.Context = fileBuf[currentByte : currentByte+chunkSize]
-		}
-		anyChunk, err := ptypes.MarshalAny(chunk)
-		if err != nil {
-			return errors.Wrap(err, "cant marshal to any")
-		}
-		req.Context = anyChunk
-		const connRetryCount = 10
-		if err := retry.Do(
-			func() error {
-				if err := stream.Send(req); err != nil {
-					log.Printf("[SendToOtherDevices client] failed to send chunk (error: %v)", err)
-					return err
-				}
-				return nil
-			},
-			retry.DelayType(func(n uint, config *retry.Config) time.Duration {
-				log.Printf("[SendToOtherDevices client] retry to send because failed to send buffer ")
-				return time.Second
-			}),
-			retry.Attempts(connRetryCount),
-		); err != nil {
-			return err
-		}
-	}
-	log.Printf("[SendToOtherDevices client] finish to send file: %s, %s", fileName, filePath)
-	return nil
-}
-
 // callback that server receive data from other client
 func (srv *Server) SendToOtherDevices(stream kanbanpb.SendAnything_SendToOtherDevicesServer) error {
 	log.Printf("[SendToOtherDevices server] create connection")
-	ctx := stream.Context()
-	message, code := srv.messageParserInSendAnythingServer(ctx, stream, srv.env.GetDataDir())
+	message, code := srv.messageParserInSendAnythingServer(stream, srv.env.GetDataDir())
 	resp := &kanbanpb.UploadStatus{
 		Message:    "[SendToOtherDevices server] " + message,
 		StatusCode: code,
@@ -320,8 +149,7 @@ func (srv *Server) SendToOtherDevices(stream kanbanpb.SendAnything_SendToOtherDe
 }
 
 // parsing message that received from send anything client in remote devices
-func (srv *Server) messageParserInSendAnythingServer(ctx context.Context,
-	stream kanbanpb.SendAnything_SendToOtherDevicesServer, dirPath string) (string, kanbanpb.UploadStatusCode) {
+func (srv *Server) messageParserInSendAnythingServer(stream kanbanpb.SendAnything_SendToOtherDevicesServer, dirPath string) (string, kanbanpb.UploadStatusCode) {
 
 	// received message var
 	kContainer := &kanbanpb.SendKanban{}
@@ -329,98 +157,72 @@ func (srv *Server) messageParserInSendAnythingServer(ctx context.Context,
 
 	// output file var
 	var recvFileBuf []byte
-	var fileName string
-
 	numOfOutputFile := 0
-
-	recvCh := make(chan *kanbanpb.SendContext, 1)
-	go func() {
-		for {
-			// wait stream by client
-			in, err := stream.Recv()
-			if err != nil {
-				if err != io.EOF {
-					log.Printf("[SendToOtherDevices server] received err: %v", err)
-				}
-				recvCh <- &kanbanpb.SendContext{Code: -1}
-				return
-			}
-			fmt.Printf("%s", in.Code)
-			recvCh <- in
-		}
-	}()
 
 	for {
 		// wait stream by client
-		select {
-		case <-ctx.Done():
+		in, err := stream.Recv()
+		if err != nil {
 			return fmt.Sprintf("cant receive message"), kanbanpb.UploadStatusCode_Failed
-		case in := <-recvCh:
-			switch in.Code {
-			// receive kanban
-			case kanbanpb.UploadRequestCode_SendingKanban:
-				if err := ptypes.UnmarshalAny(in.Context, kContainer); err != nil {
-					return fmt.Sprintf("cant unmarshal any, %v", err), kanbanpb.UploadStatusCode_Failed
+		}
+		fmt.Printf("%s", in.Code)
+		switch in.Code {
+		// receive kanban
+		case kanbanpb.UploadRequestCode_SendingKanban:
+			if err := ptypes.UnmarshalAny(in.Context, kContainer); err != nil {
+				return fmt.Sprintf("cant unmarshal any, %v", err), kanbanpb.UploadStatusCode_Failed
+			}
+			dirPath = common.GetMsDataPath(dirPath, kContainer.NextService, int(kContainer.NextNumber))
+			if f, err := os.Stat(dirPath); os.IsNotExist(err) || !f.IsDir() {
+				if err := os.Mkdir(dirPath, 0775); err != nil {
+					log.Printf("[SendToOtherDevices server] cant create directory, %v", err)
 				}
-				dirPath = common.GetMsDataPath(dirPath, kContainer.NextService, int(kContainer.NextNumber))
-				if f, err := os.Stat(dirPath); os.IsNotExist(err) || !f.IsDir() {
-					if err := os.Mkdir(dirPath, 0775); err != nil {
-						log.Printf("[SendToOtherDevices server] cant create directory, %v", err)
-					}
-				}
-				log.Printf("[SendToOtherDevices server] receive kanban (nextService: %s)", kContainer.NextService)
+			}
+			log.Printf("[SendToOtherDevices server] receive kanban (nextService: %s)", kContainer.NextService)
 
 			// receive file
-			case kanbanpb.UploadRequestCode_SendingFile_CONT, kanbanpb.UploadRequestCode_SendingFile_EOF:
-				if err := ptypes.UnmarshalAny(in.Context, chunk); err != nil {
-					return fmt.Sprintf("cant unmarshal any, %v", err), kanbanpb.UploadStatusCode_Failed
-				}
-				fileName = ""
-				if fileName == "" {
-					fileName = chunk.Name
-				} else if fileName != chunk.Name {
-					return fmt.Sprintf("receive other file before eos received (before: %s, after: %s)",
-						fileName, chunk.Name), kanbanpb.UploadStatusCode_Failed
-				}
-				recvFileBuf = append(recvFileBuf, chunk.Context...)
-				if in.Code == kanbanpb.UploadRequestCode_SendingFile_EOF {
-					fileName := filepath.Base(fileName)
-					filePath := path.Join(dirPath, fileName)
-					fileDirPath := path.Dir(filePath)
-					if f, err := os.Stat(fileDirPath); os.IsNotExist(err) || !f.IsDir() {
-						if err := os.MkdirAll(fileDirPath, 0775); err != nil {
-							log.Printf("[SendToOtherDevices server] cant create directory, %v", err)
-						}
-					}
-					f, err := os.Create(filePath)
-					if err != nil {
-						return fmt.Sprintf("cant open output path: %v", err),
-							kanbanpb.UploadStatusCode_Failed
-					}
-					if _, err := f.Write(recvFileBuf); err != nil {
-						return fmt.Sprintf("cant write output file: %v", err),
-							kanbanpb.UploadStatusCode_Failed
-					}
-					log.Printf("[SendToOtherDevices server] success to write file (%s)", filePath)
-					fileName = ""
-					recvFileBuf = []byte{}
-					numOfOutputFile += 1
-				}
-			// receive eos
-			case kanbanpb.UploadRequestCode_EOS:
-				if numOfOutputFile != len(kContainer.AfterKanban.FileList) {
-					return fmt.Sprintf("enough files haven't received yet"), kanbanpb.UploadStatusCode_Failed
-				}
-				srv.sendToServiceBrokerCh <- kContainer
-				return fmt.Sprintf("all files are received"), kanbanpb.UploadStatusCode_OK
-			// receive eof from stream
-			default:
-				if numOfOutputFile != len(kContainer.AfterKanban.FileList) {
-					return fmt.Sprintf("enough files haven't received yet"), kanbanpb.UploadStatusCode_Failed
-				}
-				srv.sendToServiceBrokerCh <- kContainer
-				return fmt.Sprintf("all files are received"), kanbanpb.UploadStatusCode_OK
+		case kanbanpb.UploadRequestCode_SendingFile_CONT:
+			if err := ptypes.UnmarshalAny(in.Context, chunk); err != nil {
+				return fmt.Sprintf("cant unmarshal any, %v", err), kanbanpb.UploadStatusCode_Failed
 			}
+			recvFileBuf = append(recvFileBuf, chunk.Context...)
+		case kanbanpb.UploadRequestCode_SendingFile_EOF:
+			fileName := filepath.Base(chunk.Name)
+			filePath := path.Join(dirPath, fileName)
+			if f, err := os.Stat(dirPath); os.IsNotExist(err) || !f.IsDir() {
+				if err := os.MkdirAll(dirPath, 0775); err != nil {
+					log.Printf("[SendToOtherDevices server] cant create directory, %v", err)
+				}
+			}
+			f, err := os.Create(filePath)
+			if err != nil {
+				return fmt.Sprintf("cant open output path: %v", err),
+					kanbanpb.UploadStatusCode_Failed
+			}
+			if _, err := f.Write(recvFileBuf); err != nil {
+				f.Close()
+				return fmt.Sprintf("cant write output file: %v", err),
+					kanbanpb.UploadStatusCode_Failed
+			}
+			log.Printf("[SendToOtherDevices server] success to write file (%s)", filePath)
+			fileName = ""
+			recvFileBuf = []byte{}
+			numOfOutputFile += 1
+			f.Close()
+		// receive eos
+		case kanbanpb.UploadRequestCode_EOS:
+			if numOfOutputFile != len(kContainer.AfterKanban.FileList) {
+				return fmt.Sprintf("enough files haven't received yet"), kanbanpb.UploadStatusCode_Failed
+			}
+			srv.sendToServiceBrokerCh <- kContainer
+			return fmt.Sprintf("all files are received"), kanbanpb.UploadStatusCode_OK
+		// receive eof from stream
+		default:
+			if numOfOutputFile != len(kContainer.AfterKanban.FileList) {
+				return fmt.Sprintf("enough files haven't received yet"), kanbanpb.UploadStatusCode_Failed
+			}
+			srv.sendToServiceBrokerCh <- kContainer
+			return fmt.Sprintf("all files are received"), kanbanpb.UploadStatusCode_OK
 		}
 	}
 }
